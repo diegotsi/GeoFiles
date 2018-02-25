82e7352c940efefd9facd9cf8f51d4625a2ce14c
vitorkaio <vitorcaiodepaula@gmail.com>
2018-02-25 12:49:46 -0300

    Metódo para cadastrar usuário adicionado.

--- app/services/ApiServices.js
+++ app/services/ApiServices.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const urlIsLogged = "https://agile-cliffs-74468.herokuapp.com/drop/user/logged";
+const urlRegisterUser = "https://agile-cliffs-74468.herokuapp.com/drop/user/register";
 
 /**
  * Classe que gerencia o acesso ao serviços da API.
@@ -21,7 +22,7 @@ class ApiService {
    * @memberof ApiService
    */
   static isRegister(name, pass) {
-    const user = { name, pass };
+    const user = {name: name, pass: pass };
     return new Promise((resolve, reject) => {
       axios.post(urlIsLogged, user).then(res => {
         if (res.data['msg'] === 1) {
@@ -39,6 +40,38 @@ class ApiService {
       });
     });
   } // Fim do método.
+
+
+  /**
+   * Cadastra um usuário no sistema.
+   * 
+   * @static
+   * @param {any} name nome do usuário vindo do input
+   * @param {any} pass senha do usuário vindo do input.
+   * @returns {Promise} (msg: 1, data: dados) se o usuário foi cadastrado com sucesso,
+   * (msg: 0, data: 'usuário já cadastrado') se um usuário já estiver cadastrado com o nome passado.
+   * (msg: 501, data: null) se algum erro ocorreu no servidor, no firebase ou no webapp.
+   * @memberof ApiService
+   */
+  static registerUser(name, pass) {
+    const user = { name, pass };
+    // console.log('www: ', user);
+    return new Promise((resolve, reject) => {
+      axios.post(urlRegisterUser, user).then(res => {
+        console.log(res);
+        if (res.data['msg'] === 1) {
+          resolve({ msg: 1, data: res.data['data'] });
+        }
+        else if (res.data['msg'] === 0)
+          resolve({ msg: 0, data: res.data['data'] });
+        else
+          reject({ msg: 501, data: null });
+      }).catch(err => {
+        console.log(err);
+        reject({ msg: 501, data: null });
+      });
+    });
+  }
 
 }// Fim da classe
 
