ce91daec429d8c9167a942b5d49f88fdd9bdd6bc
vitorkaio <vitorcaiodepaula@gmail.com>
2018-02-25 11:30:26 -0300

    Adicionei uma classe para acessar API.

---
+++ app/services/ApiServices.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const urlIsLogged = "https://agile-cliffs-74468.herokuapp.com/drop/user/logged";
+
+/**
+ * Classe que gerencia o acesso ao serviços da API.
+ * https://agile-cliffs-74468.herokuapp.com/
+ * 
+ * @class ApiService
+ */
+class ApiService {
+/**
+ * Verifica se um usuário está cadastrado no sistema.
+ * 
+ * @static
+ * @param {any} name nome do usuário vindo do input do formulário.
+ * @param {any} pass senha do """" """".
+ * @returns {Promise} resolve(msg: 1, data: dados) se o usuário estiver cadastrado, resolve(msg: 0) se o usuário 
+ * está logado mas a senha está errada, resolve(msg: -1) se o usuário não está cadastrado, reject(msg: 501) se a 
+ * operação não foi possível.
+ * @memberof ApiService
+ */
+static isRegister(name, pass) {
+    const user = {name, pass};
+    return new Promise((resolve, reject) => {
+      axios.post(urlIsLogged, user).then(res => {
+        if(res.data.msg === 1) {
+          const res = {data: res.data.data};
+          resolve({msg: 1, data: res});
+        }
+        else if(res.data.msg === 0) {
+          resolve({msg: 0});
+        }
+        else if(res.data.msg === -1)
+          resolve({msg: -1});
+        else 
+          reject({msg: 501});
+      }).catch(err => {
+        reject({msg: 501});
+      });
+    });
+  } // Fim do método.
+
+}// Fim da classe
