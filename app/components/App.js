7106578aecb2f55816697002318788c0330fedee
vitorkaio <vitorcaiodepaula@gmail.com>
2018-02-25 11:31:14 -0300

    Mudei a estrutura do arquivo para verificar o acesso a API.

--- app/components/App.js
+++ app/components/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import img from '../assets/images/react_logo_512x512.png';
+import React, { Component } from 'react';
+// import img from '../assets/images/react_logo_512x512.png';
 
-const App = () => {
-  return (
-    <div>
-      <h2 id="heading">Hello ReactJS</h2>
-      <img
-        className="image"
-        style={{ margin: '0.5em' }}
-        height="40"
-        width="40"
-        src={img}
-        alt="React Logo"
-      />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { nameInput: '', passInput: '' };
+  }
+
+  // Input name.
+  nameInput = (e) => {
+    this.setState({ nameInput: e.target.value });
+  };
+
+  // Pass name.
+  passInput = (e) => {
+    this.setState({ passInput: e.target.value });
+  }
+
+  // Submit.
+  submit = () => {
+    console.log('Entrada: ', this.state.nameInput, this.state.passInput);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.nameInput} value={this.state.nameInput} placeholder="Usuário" />
+        <input type="text" onChange={this.passInput} value={this.state.passInput} placeholder="Senha" />
+        <button onClick={this.submit}>Login</button>
+      </div>
+    );
+  }
+
+}// Fim do component.
 
 export default App;
